8b09f9182d71d17061732617e36f8f3aa259392d
Ian Sillitoe <ian@sillit.com>
2020-06-25 20:43:04 +0100

    add autogen swagger app to main app

--- bio3dbeacon/app.py
+++ bio3dbeacon/app.py
@@ -1,5 +1,6 @@
 import logging
 
+import connexion
 from flask import Flask, Blueprint
 from flask_migrate import Migrate
 
@@ -8,6 +9,7 @@ from bio3dbeacon.api.query.endpoints import ns as query_ns
 from bio3dbeacon.api.restx import api
 from bio3dbeacon.database import db
 from bio3dbeacon.frontend.frontend import frontend_bp
+from swagger_server import encoder
 
 LOG = logging.getLogger(__name__)
 
@@ -42,6 +44,14 @@ def initialise_app(flask_app):
     flask_app.register_blueprint(api_bp)
 
     flask_app.register_blueprint(frontend_bp)
+
+    swagger_app = connexion.FlaskApp(__name__.split('.')[0])
+    swagger_app.app.json_encoder = encoder.JSONEncoder
+    swagger_app.add_api('../swagger_server/swagger/swagger.yaml')
+
+    for bp in swagger_app.app.iter_blueprints():
+        LOG.info("swagger blueprint: %s", bp)
+        flask_app.register_blueprint(bp, url_prefix='/client')
 
 
 def flask_cli():
