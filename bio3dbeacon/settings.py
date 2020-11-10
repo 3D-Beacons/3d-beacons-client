a534d6de083df8a173677e05de6928c597a44679
Ian Sillitoe <ian@sillit.com>
2020-11-10 15:10:39 +0000

    use prettyconf

--- bio3dbeacon/settings.py
+++ bio3dbeacon/settings.py
@@ -1,28 +1,29 @@
 import os
 from pathlib import Path
+from prettyconf import config
 
 BEACON_NAME = 'ModelsRUs (London)'
 
-FLASK_SERVER_NAME = 'localhost:5000'
-FLASK_DEBUG = True
 
 RESTX_SWAGGER_UI_DOC_EXPANSION = 'list'
 RESTX_VALIDATE = True
 RESTX_MASK_SWAGGER = False
 RESTX_ERROR_404_HELP = False
 
-DATABASE_DRIVER = os.environ.get('DATABASE_DRIVER', 'postgresql')
-DATABASE_HOST = os.environ.get('DATABASE_HOST', 'localhost')
-DATABASE_NAME = os.environ.get('DATABASE_NAME', 'bio3dbeacon_client')
-DATABASE_READER_USER = os.environ.get(
-    'DATABASE_READER_USER', 'bio3dbeacon_reader')
-DATABASE_READER_PASSWORD = os.environ.get(
-    'DATABASE_READER_PASSWORD', 'bio3dbeacon_reader')
-DATABASE_WRITER_USER = os.environ.get(
-    'DATABASE_WRITER_USER', 'bio3dbeacon_writer')
-DATABASE_WRITER_PASSWORD = os.environ.get('DATABASE_WRITER_PASSWORD', None)
+DATABASE_DRIVER = config('DATABASE_DRIVER', default='postgresql')
+DATABASE_HOST = config('DATABASE_HOST', default='localhost')
+DATABASE_NAME = config('DATABASE_NAME')
+DATABASE_PORT = config('DATABASE_PORT', default='5432')
+DATABASE_READER_USER = config(
+    'DATABASE_READER_USER', default='bio3dbeacon_reader')
+DATABASE_READER_PASSWORD = config(
+    'DATABASE_READER_PASSWORD', default='bio3dbeacon_reader')
+DATABASE_WRITER_USER = config('DATABASE_WRITER_USER')
+DATABASE_WRITER_PASSWORD = config('DATABASE_WRITER_PASSWORD')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SQLALCHEMY_DATABASE_URI = f'{DATABASE_DRIVER}://{DATABASE_WRITER_USER}:{DATABASE_WRITER_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
 
 ROOT_DIR = (Path(__file__) / '..' / '..').resolve()
 MOLSTAR_GITHUB_URL = 'https://github.com/molstar/molstar.git'
@@ -33,4 +34,5 @@ WORK_DIR = ROOT_DIR / 'work_dir'
 MOLSTAR_PREPROCESS_EXE = ROOT_DIR / 'molstar' / \
     'build' / 'model-server' / 'preprocess.js'
 
-SECRET_KEY = 'fourteen-ants-marching-over-mushrooms'
+SECRET_KEY = config(
+    'SECRET_KEY', default='fourteen-ants-marching-over-mushrooms')
