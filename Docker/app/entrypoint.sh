80bf4da87e450a15996892083117f2fa2d268734
Ian Sillitoe <ian@sillit.com>
2020-11-10 15:11:59 +0000

    move all docker stuff to ./Docker

--- Docker/app/entrypoint.sh
+++ Docker/app/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-sleep 10
+if [ "$DATABASE_TYPE" = "postgres" ]
+then
+    echo "Waiting for postgres to start (host=$DATABASE_HOST, port=$DATABASE_PORT)..."
+
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+        sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
 
 flask db upgrade
 flask db migrate
