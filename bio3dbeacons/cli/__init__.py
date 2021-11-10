9177e54dbdf1088b8b47ca265b7260835a4a382a
Ian Sillitoe <ian@sillit.com>
2021-11-10 16:16:28 +0000

    split long line

--- bio3dbeacons/cli/__init__.py
+++ bio3dbeacons/cli/__init__.py
@@ -14,7 +14,8 @@ try:
     log_level = os.getenv("LOG_LEVEL", "INFO").upper()
     root.setLevel(log_level)
     if log_level == 'DEBUG':
-        log_format = "%(asctime)s %(module)s:%(lineno)d[%(process)d] %(levelname)6s %(message)s"
+        log_format = ("%(asctime)s %(module)s:%(lineno)d[%(process)d] "
+                      "%(levelname)6s %(message)s")
 
 except ValueError:
     root.error("Invalid log level in env var LOG_LEVEL. Defaulting to INFO")
