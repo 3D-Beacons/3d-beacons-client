0e0a954b787efd123432f19d983e3cf6b3078df8
Ian Sillitoe <ian@sillit.com>
2022-03-08 22:24:42 +0000

    url should not include host

--- tests/tests_api/test_utils.py
+++ tests/tests_api/test_utils.py
@@ -4,8 +4,8 @@ from bio3dbeacons.api import utils
 class TestUtils:
     def test_get_model_asset_url_default(self):
         result = utils.get_model_asset_url("someEntryId")
-        assert result == "localhost/static/cif/someEntryId.cif"
+        assert result == "/static/cif/someEntryId.cif"
 
     def test_get_model_asset_url_pdb(self):
         result = utils.get_model_asset_url("someEntryId", "pdb")
-        assert result == "localhost/static/pdb/someEntryId.pdb"
+        assert result == "/static/pdb/someEntryId.pdb"
